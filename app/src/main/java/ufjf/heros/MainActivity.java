42e65cbae48388b751401a9ee7b6818726fae8b8
Pedro Antonio Bittencourt <pedroantonio.bittencourt@gmail.com>
2016-10-17 18:09:18 -0200

    Criada a activity de criar herói

--- app/src/main/java/ufjf/heros/MainActivity.java
+++ app/src/main/java/ufjf/heros/MainActivity.java
@@ -1,13 +1,27 @@
 package ufjf.heros;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCreateHeroActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCreateHeroActivity = (Button) findViewById(R.id.btnCreateHeroActivity);
+        btnCreateHeroActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(view.getContext(),CreateHeroActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
